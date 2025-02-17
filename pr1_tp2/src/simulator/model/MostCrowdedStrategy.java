8b8a0987ff409d10cb66c8a837977eefce7bfe91
Glen843 <Pabligenio3.0@gmail.com>
2025-02-17 21:07:50 +0100

    Update MostCrowdedStrategy.java
    Posibles cambios en el metodo circularSearch

--- pr1_tp2/src/simulator/model/MostCrowdedStrategy.java
+++ pr1_tp2/src/simulator/model/MostCrowdedStrategy.java
@@ -5,7 +5,8 @@ import java.util.List;
 public class MostCrowdedStrategy implements LightSwitchingStrategy{
 
 	private int _timeSlot;
-	
+
+	//Constructor
 	public MostCrowdedStrategy(int timeSlot) {
 		
 		_timeSlot = timeSlot;
@@ -16,28 +17,24 @@ public class MostCrowdedStrategy implements LightSwitchingStrategy{
 	public int chooseNextGreen(List<Road> roads, List<List<Vehicle>> qs, int currGreen, int lastSwitchingTime,
 			int currTime) {
 		
-		//devuelve la carretera entrante con la cola más larga, realizando una búsqueda circular (en qs) 
-		//desde la posición currGreen+1 módulo el número de carreteras entrantes al cruce
-		int res = circularSearch((currGreen+1) % roads.size(), qs);;
+		// 1.-
+		if(roads.isEmpty()) return -1;
 		
-		//si la lista de carreteras entrantes es vacı́a, entonces devuelve -1
-		if(roads.isEmpty()) res = -1;
+		//2.-
+		if (currGreen == -1) return circularSearch(qs,0);
 		
-		//si todos los semáforos de las carreteras entrantes están en rojo, pone verde 
-		//el semáforo de la carretera entrante con la cola más larga empezando en 0
-		else if(currGreen == -1) res = circularSearch(0, qs);
+		//3.-
+		if((currTime - lastSwitchingTime) < _timeSlot) return currGreen;
 		
-		//si currTime-lastSwitchingTime < timeSlot, devuelve currGreen
-		else if(currTime-lastSwitchingTime < _timeSlot) res = currGreen;
-		
-		return res;
+		//4.-
+		return circularSearch(qs,(currGreen + 1)%roads.size());
 	}
-	
+	//Metodo mal planteado
 	private int circularSearch(int starting_pos, List<List<Vehicle>> qs) {
 		
 		int max = 0;
 		int i_max = 0;
-		
+		//Solo recorres desde la posicion inicial hasta el final, si starting_pos = 2 y el tamaño de qs es 4 solo recorrerias 2 elementos ultimos
 		for(int i = starting_pos; i < qs.size(); ++i) {
 			int size = qs.get(i).size();
 			if(size > max) {
@@ -48,5 +45,26 @@ public class MostCrowdedStrategy implements LightSwitchingStrategy{
 		
 		return i_max;
 	}
+
+	//Metodo propuesto
+	//Metodo encontrar lista mas larga de manera circular
+	private int circularSearch(List<List<Vehicle>> qs,int startIndex) {
+		int maxQueue = -1;
+		int selectRoad = startIndex;
+		
+		//Recorre la lista de manera circular
+		for(int i = 0; i<qs.size(); ++i) {
+			
+			int index = (startIndex+i)%qs.size();
+			
+			//Si encontramos una cola mas grande se actualiza, si hay dos iguales la primera prevalece
+			if(qs.get(index).size() > maxQueue) {
+				maxQueue = qs.get(index).size();
+				selectRoad = index;
+			}
+		}
+		return selectRoad;
+	}
+
 	
 }
