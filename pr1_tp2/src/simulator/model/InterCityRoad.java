00d445cbc37268aeed7bedcba4ddd2bc1545a28b
Glen843 <Pabligenio3.0@gmail.com>
2025-02-17 20:29:36 +0100

    OK

--- pr1_tp2/src/simulator/model/InterCityRoad.java
+++ pr1_tp2/src/simulator/model/InterCityRoad.java
@@ -2,10 +2,11 @@ package simulator.model;
 
 public class InterCityRoad extends Road{
 
+	//Constructor
 	InterCityRoad(String id, Junction srcJunc, Junction destJunc, int maxSpeed, int contLimit, int length,
 			Weather weather) {
 		super(id, srcJunc, destJunc, maxSpeed, contLimit, length, weather);
-		// TODO Auto-generated constructor stub
+		
 	}
 	
 	public void reduceTotalContamination(){
